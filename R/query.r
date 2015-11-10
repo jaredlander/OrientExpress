c6c572e6ab7048fc3dbb6b0d04e0c8cde0618b58
Jared Lander <info@jaredlander.com>
2015-11-10 01:41:28 -0500

    Added a format function to handle NA dates.

--- R/query.r
+++ R/query.r
@@ -1,7 +1,5 @@
 #' @param limit The limit for the number of records; defualt is -1 for all records
-myDB <- dbInfo('162.223.15.109', database='GratefulDeadConcerts', username='admin', password='admin', port='2480')
-buildQuery(myDB, 'select * from V', 20)
-mine <- query(myDB, 'select * from V', 20)
+
 content(mine)
 buildQuery <- function(db, query, limit=-1)
 {
@@ -11,4 +9,9 @@ buildQuery <- function(db, query, limit=-1)
 query <- function(db, query, limit=-1)
 {
     GET(buildQuery(db, query, limit))
+}
+
+format.logical <- function(x, ...)
+{
+    x
 }
